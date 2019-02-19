cbb006a750649c2c28ace236fb3705a4b1bfe70b
mperry <martin.perry@patientco.com>
2019-02-18 19:31:56 -0500

    first two tests

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -14,4 +14,31 @@ public class GildedRoseTest {
         assertEquals("fixme", app.items[0].name);
     }
 
+    @Test
+    public void BrieGoesUp(){
+        Item[] brie = new Item[]{new Item("Aged Brie", 2, 0)};
+        GildedRose gr = new GildedRose(brie);
+        gr.updateQuality();
+        assertEquals(1, brie[0].quality);
+    }
+
+    @Test
+    public void SulfurasStaystheSame(){
+        Item[] Sulfuras = new Item[]{new Item("Sulfuras, Hand of Ragnaros", 2, 80)};
+        GildedRose gr = new GildedRose(Sulfuras);
+        gr.updateQuality();
+        assertEquals(80, Sulfuras[0].quality);
+        for(int i=0 ; i<= 80 ; i++) {
+            gr.updateQuality();
+        }
+        assertEquals(80, Sulfuras[0].quality);
+    }
+
+    @Test
+    public void SulfurasIs80(){
+        Item[] Sulfuras = new Item[]{new Item("Sulfuras, Hand of Ragnaros", 2, 0)};
+        GildedRose gr = new GildedRose(Sulfuras);
+        gr.updateQuality();
+        assertEquals(80, Sulfuras[0].quality);
+    }
 }
