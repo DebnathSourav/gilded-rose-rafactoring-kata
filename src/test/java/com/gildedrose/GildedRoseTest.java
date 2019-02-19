e18042338dabaa0c2c5799447fed258509ccb774
mperry <martin.perry@patientco.com>
2019-02-18 19:32:20 -0500

    removed garbage test

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -7,14 +7,6 @@ import org.junit.Test;
 public class GildedRoseTest {
 
     @Test
-    public void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
-    }
-
-    @Test
     public void BrieGoesUp(){
         Item[] brie = new Item[]{new Item("Aged Brie", 2, 0)};
         GildedRose gr = new GildedRose(brie);
